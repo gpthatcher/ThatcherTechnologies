fe5594108c7dc46c5ebb07a42d8c548c3497fe14
gpthatcer <gregthatcher@thatchertechnologies.com>
2024-09-24 00:36:50 -0400

    changing astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,29 +6,24 @@ import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-  // https://docs.astro.build/en/guides/images/#authorizing-remote-images
-  site: "https://thatchertechnologies.uk",
+  // Base path is always `/` since both GitHub Pages and Production use the same root domain.
+  base: "/",
+
+  site: "https://www.thatchertechnologies.com", // Your custom domain (used for generating sitemaps, metadata, etc.)
+
   image: {
     domains: ["images.unsplash.com"],
   },
-  // i18n: {
-  //   defaultLocale: "en",
-  //   locales: ["en", "fr"],
-  //   fallback: {
-  //     fr: "en",
-  //   },
-  //   routing: {
-  //     prefixDefaultLocale: false,
-  //   },
-  // },
+
   prefetch: true,
+
   integrations: [
     tailwind(),
     sitemap({
       i18n: {
-        defaultLocale: "en", // All urls that don't contain `fr` after `https://thatchertechnologies.uk/` will be treated as default locale, i.e. `en`
+        defaultLocale: "en",
         locales: {
-          en: "en", // The `defaultLocale` value must present in `locales` keys
+          en: "en",
           fr: "fr",
         },
       },
@@ -36,11 +31,6 @@ export default defineConfig({
     starlight({
       title: "ThatcherTechnologies Docs",
       defaultLocale: "root",
-      // https://github.com/withastro/starlight/blob/main/packages/starlight/CHANGELOG.md
-      // If no Astro and Starlight i18n configurations are provided, the built-in default locale is used in Starlight and a matching Astro i18n configuration is generated/used.
-      // If only a Starlight i18n configuration is provided, an equivalent Astro i18n configuration is generated/used.
-      // If only an Astro i18n configuration is provided, the Starlight i18n configuration is updated to match it.
-      // If both an Astro and Starlight i18n configurations are provided, an error is thrown.
       locales: {
         root: {
           label: "English",
@@ -53,7 +43,6 @@ export default defineConfig({
         ja: { label: "日本語", lang: "ja" },
         "zh-cn": { label: "简体中文", lang: "zh-CN" },
       },
-      // https://starlight.astro.build/guides/sidebar/
       sidebar: [
         {
           label: "Quick Start Guides",
@@ -84,7 +73,7 @@ export default defineConfig({
         },
       ],
       social: {
-        github: "https://github.com/mearashadowfax/ThatcherTechnologies",
+        github: "https://github.com/gpthatcher/ThatcherTechnologies",
       },
       disable404Route: true,
       customCss: ["./src/assets/styles/starlight.css"],
@@ -100,14 +89,14 @@ export default defineConfig({
           tag: "meta",
           attrs: {
             property: "og:image",
-            content: "https://thatchertechnologies.uk" + "/social.webp",
+            content: "https://www.thatchertechnologies.com/social.webp",
           },
         },
         {
           tag: "meta",
           attrs: {
             property: "twitter:image",
-            content: "https://thatchertechnologies.uk" + "/social.webp",
+            content: "https://www.thatchertechnologies.com/social.webp",
           },
         },
       ],
@@ -117,7 +106,9 @@ export default defineConfig({
       brotli: true,
     }),
   ],
+
   output: "static",
+
   experimental: {
     clientPrerender: true,
     directRenderScript: true,
